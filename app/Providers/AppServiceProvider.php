b431269b21b31aa2870dae2bb8e1616ca6a17e6e
zhuli <18652968364@163.com>
2018-01-26 08:27:14 +0000

    数据库

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Models\PaymentDetail;
 use App\Observers\PaymentDetailObserver;
 use Encore\Admin\Config\Config;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -16,6 +17,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        Schema::defaultStringLength(191);
         //
         Config::load();
 
