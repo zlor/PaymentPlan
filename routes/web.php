6434598ed49d30bd614b35bbf45650fba1dbdaa5
zlor <731545922@qq.com>
2018-03-08 18:21:08 +0800

    暂时支持导出所有计划数据

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
-
+use \Illuminate\Support\Facades\Route;
+use  Maatwebsite\Excel\Facades\Excel;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -23,4 +24,35 @@ Route::get('/', function(){
     ];
 
     return view('bill_books', compact('books', 'map'));
+});
+
+Route::get('getExcel/{id}', function($id){
+
+    $billPeriod = \App\Models\BillPeriod::query()->findOrFail($id);
+
+    $schedules = \App\Models\PaymentSchedule::query()->where('bill_period_id', $billPeriod->id)->get();
+    $head = ["供应商名称", '类型', "总应付金额", "建议应付金额"];
+
+    $sheetData[] = $head;
+
+    foreach ($schedules as $schedule)
+    {
+        $row = [
+            0=> $schedule->supplier_name,
+            1=> $schedule->payment_type_name,
+            2=> $schedule->supplier_balance,
+            3=> $schedule->suggest_due_money,
+        ];
+        $sheetData[] = $row;
+    }
+
+    $sheetName = 'tmp';
+
+    $excel = Excel::create('tmp', function($excel)use($sheetData, $sheetName){
+
+        $excel->sheet($sheetName, function($sheet)use($sheetData) {
+
+            $sheet->fromArray($sheetData);
+        });
+    })->store('xls', storage_path('public'))->download();
 });
